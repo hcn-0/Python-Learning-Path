b59de39d4993a51326d4c84cfdced9acb4d6e150
Mehedi <mehedihasan557711@gmail.com>
2025-07-31 18:30:36 +0600

    if-elif

---
+++ CS50-Introduction-to-Python-Programming/compare.py
@@ -0,0 +1,17 @@
+x = int(input("What's X? "))
+y = int(input("What's Y? "))
+
+if x > y:
+    print("X is greater than Y")
+elif x < y:
+    print("X is less than Y")
+else:
+    print("Both are equal")
+
+
+if x != y:
+    print("Not equal")
+else:
+    print("Equal")
+
+
