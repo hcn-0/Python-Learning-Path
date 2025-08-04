f13be30d7f0bff674633a6bbfd2e34066b893a88
Mehedi <mehedihasan557711@gmail.com>
2025-08-04 17:08:50 +0600

    Dictionary

--- CS50-Introduction-to-Python-Programming/hogwarts.py
+++ CS50-Introduction-to-Python-Programming/hogwarts.py
@@ -1,8 +1,36 @@
-students = ["Hermione", "Harry", "Ron"]
+#students = ["Hermione", "Harry", "Ron", "Draco"]
 
-for std in students:
+"""for std in students:
     print(std)
 
 for i in range(len(students)):
     print(students[i])
+"""
+
+students = {
+        "Hermione": "Gryffindor",
+        "Harry": "Gryffindor",
+        "Ron": "Gryffindor",
+        "Draco": "Slytherin",
+        }
+
+
+for student in students:
+    print(student, students[student], sep = ": ")
+
+
+students1 = [
+        {"Name": "Hermione", "House": "Gryffiondor", "Patronus": "Otter"},
+        {"Name": "Harry", "House": "Gryffindor", "Patronus": "Stag"},
+        {"Name": "Ron", "House": "Gryffindor", "Patronus": "Jack Rusell terrir"},
+        {"Name": "Draco", "House": "Slytherin", "Patronus": None}
+        ]
+
+print()
+for std in students1:
+   # print(f"{std["Name"]}: {std["House"]}")
+   print(std["Name"], std["House"], std["Patronus"], sep = ", ")
+
+
+
 
